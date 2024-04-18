18d41d7cc311195ee25a409931d6c8b2d4723fda
Justin Stone <justone@davidson.edu>
2024-04-17 21:28:10 -0400

    changes to the dashboard

--- .ipynb_checkpoints/streamlit-checkpoint.py
+++ .ipynb_checkpoints/streamlit-checkpoint.py
@@ -1,342 +1,139 @@
-from pygwalker.api.streamlit import StreamlitRenderer
+from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
+import pygwalker as pyg
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
-vis_spec = """[
-  {
-    "config": {
-      "defaultAggregated": true,
-      "geoms": [
-        "poi"
-      ],
-      "coordSystem": "geographic",
-      "limit": -1,
-      "timezoneDisplayOffset": 0
-    },
-    "encodings": {
-      "dimensions": [
-        {
-          "dragId": "gw_dOue",
-          "fid": "Region Name",
-          "name": "Region Name",
-          "basename": "Region Name",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_C3og",
-          "fid": "Region Type",
-          "name": "Region Type",
-          "basename": "Region Type",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_Z_gj",
-          "fid": "State Name",
-          "name": "State Name",
-          "basename": "State Name",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_KZ6B",
-          "fid": "Date Recorded",
-          "name": "Date Recorded",
-          "basename": "Date Recorded",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_hpco",
-          "fid": "Coordinates",
-          "name": "Coordinates",
-          "basename": "Coordinates",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_ngIl",
-          "fid": "Latitude",
-          "name": "Latitude",
-          "basename": "Latitude",
-          "semanticType": "quantitative",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_Zf19",
-          "fid": "Longitude",
-          "name": "Longitude",
-          "basename": "Longitude",
-          "semanticType": "quantitative",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_mea_key_fid",
-          "fid": "gw_mea_key_fid",
-          "name": "Measure names",
-          "analyticType": "dimension",
-          "semanticType": "nominal"
-        },
-        {
-          "fid": "Coordinates_x",
-          "name": "Coordinates_x",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "basename": "Coordinates_x",
-          "dragId": "GW_VYCpk5ED"
-        },
-        {
-          "fid": "Coordinates_y",
-          "name": "Coordinates_y",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "basename": "Coordinates_y",
-          "dragId": "GW_JKRPZtl7"
-        }
-      ],
-      "measures": [
-        {
-          "dragId": "gw_MYF3",
-          "fid": "RegionID",
-          "name": "RegionID",
-          "basename": "RegionID",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "sum",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_-Y8K",
-          "fid": "SizeRank",
-          "name": "SizeRank",
-          "basename": "SizeRank",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "sum",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_r6fq",
-          "fid": "Monthly Rent",
-          "name": "Monthly Rent",
-          "basename": "Monthly Rent",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "sum",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_count_fid",
-          "fid": "gw_count_fid",
-          "name": "Row count",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "sum",
-          "computed": true,
-          "expression": {
-            "op": "one",
-            "params": [],
-            "as": "gw_count_fid"
-          }
-        },
-        {
-          "dragId": "gw_mea_val_fid",
-          "fid": "gw_mea_val_fid",
-          "name": "Measure values",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "sum"
-        },
-        {
-          "fid": "Latitude_x",
-          "name": "Latitude_x",
-          "semanticType": "quantitative",
-          "analyticType": "measure",
-          "basename": "Latitude_x",
-          "dragId": "GW_qaPKwEiy"
-        },
-        {
-          "fid": "Longitude_x",
-          "name": "Longitude_x",
-          "semanticType": "quantitative",
-          "analyticType": "measure",
-          "basename": "Longitude_x",
-          "dragId": "GW_cO9otU5l"
-        },
-        {
-          "fid": "Latitude_y",
-          "name": "Latitude_y",
-          "semanticType": "quantitative",
-          "analyticType": "measure",
-          "basename": "Latitude_y",
-          "dragId": "GW_g9hY6ZRz"
-        },
-        {
-          "fid": "Longitude_y",
-          "name": "Longitude_y",
-          "semanticType": "quantitative",
-          "analyticType": "measure",
-          "basename": "Longitude_y",
-          "dragId": "GW_M3jWtosp"
-        }
-      ],
-      "rows": [],
-      "columns": [],
-      "color": [
-        {
-          "dragId": "gw_nFfb",
-          "fid": "Monthly Rent",
-          "name": "Monthly Rent",
-          "basename": "Monthly Rent",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "mean",
-          "offset": 0
-        }
-      ],
-      "opacity": [],
-      "size": [
-        {
-          "dragId": "gw_gdUr",
-          "fid": "Monthly Rent",
-          "name": "Monthly Rent",
-          "basename": "Monthly Rent",
-          "analyticType": "measure",
-          "semanticType": "quantitative",
-          "aggName": "mean",
-          "offset": 0
-        }
-      ],
-      "shape": [],
-      "radius": [],
-      "theta": [],
-      "longitude": [
-        {
-          "dragId": "gw_EXfj",
-          "fid": "Longitude",
-          "name": "Longitude",
-          "basename": "Longitude",
-          "semanticType": "quantitative",
-          "analyticType": "dimension",
-          "offset": 0
-        }
-      ],
-      "latitude": [
-        {
-          "dragId": "gw_lgVz",
-          "fid": "Latitude",
-          "name": "Latitude",
-          "basename": "Latitude",
-          "semanticType": "quantitative",
-          "analyticType": "dimension",
-          "offset": 0
-        }
-      ],
-      "geoId": [],
-      "details": [
-        {
-          "dragId": "gw_qtvl",
-          "fid": "Date Recorded",
-          "name": "Date Recorded",
-          "basename": "Date Recorded",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        },
-        {
-          "dragId": "gw_Xulw",
-          "fid": "State Name",
-          "name": "State Name",
-          "basename": "State Name",
-          "semanticType": "nominal",
-          "analyticType": "dimension",
-          "offset": 0
-        }
-      ],
-      "filters": [],
-      "text": []
-    },
-    "layout": {
-      "showActions": false,
-      "showTableSummary": false,
-      "stack": "stack",
-      "interactiveScale": false,
-      "zeroScale": true,
-      "background": "",
-      "size": {
-        "mode": "auto",
-        "width": 800,
-        "height": 600
-      },
-      "format": {},
-      "geoKey": "name",
-      "resolve": {
-        "x": false,
-        "y": false,
-        "color": false,
-        "opacity": false,
-        "shape": false,
-        "size": false
-      },
-      "scaleIncludeUnmatchedChoropleth": false,
-      "colorPalette": "reds",
-      "useSvg": false,
-      "scale": {
-        "opacity": {},
-        "size": {}
-      },
-      "geoMapTileUrl": "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    },
-    "visId": "gw_LtRJ",
-    "name": "Chart 1"
-  }
-]"""
+# Read and preprocess data
+rental_sales = pd.read_csv("Cleaned Data/state_rentals_excluding_hawaii_alaska.csv")
+rental_sales['Date Recorded'] = pd.to_datetime(rental_sales['Date Recorded'])
+median_sales = pd.read_csv('Cleaned Data/median_sales_excluding_hawaii_alaska.csv')
+predicted_sales = pd.read_csv('Cleaned Data/cleaned_state_home_forecast.csv')
 
+# Streamlit page configuration
+st.set_page_config(page_title="Zillow Data Analysis", layout="wide", page_icon="zillow.png")
+init_streamlit_comm()
 
-# Adjust the width of the Streamlit page
-st.set_page_config(
-    page_title="Use Pygwalker In Streamlit",
-    layout="wide"
-)
+# LOGO
+logo_path = "zillow.png"
+st.image(logo_path, width=50)
 
-# Add Title
-st.title("Use Pygwalker In Streamlit")
+# Custom CSS 
+st.markdown("""
+<style>
+body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+h1 {
+    color: #0b4f6c;
+    background-color: #f0f0f0;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 20px 0;
+}
+h2 {
+    color: #01baef;
+    background-color: #f0f0f0;
+    padding: 15px;
+    border-radius: 10px;
+    margin: 15px 0;
+}
+div.stButton > button:first-child {
+    background-color: #f0f0f0;
+    color: #ffffff;
+    border-radius: 5px;
+    padding: 10px 20px;
+}
+div.streamlit-container {
+    margin: auto;
+}
+</style>
+""", unsafe_allow_html=True)
 
-# You should cache your pygwalker renderer, if you don't want your memory to explode
+# Main Title
+st.title("Zillow Data Analysis Dashboard")
+
+# Dashboard Introduction
+st.markdown("This dashboard provides a visual analysis of rental and sales data from Zillow. Scroll down to explore different visualizations and insights.")
+
+# Renderer setup
 @st.cache_resource
-def get_pyg_renderer() -> "StreamlitRenderer":
-    rental_sales = pd.read_csv('Cleaned Data/state_rentals_excluding_hawaii_alaska.csv')
-    # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
-    return StreamlitRenderer(rental_sales, spec=vis_spec, spec_io_mode="rw")
+def get_pyg_renderer(data_path: str, spec_path: str) -> "StreamlitRenderer":
+    data = pd.read_csv(data_path)
+    return StreamlitRenderer(data, spec=spec_path, debug=False)
 
+# Rental and sales price renderers
+renderer = get_pyg_renderer("Cleaned Data/2023_rental_price.csv", "rentals_2023.json")
+renderer2 = get_pyg_renderer("Cleaned Data/2023_sales_state.csv", "median_sales_2023.json")
+renderer3 = get_pyg_renderer("Cleaned Data/2023_sale_price.csv", "geographic_2023_sales.json")
 
-renderer = get_pyg_renderer()
-
-st.subheader("Display Explore UI")
-
-tab1, tab2, tab3, tab4 = st.tabs(
-    ["graphic walker", "data profiling", "graphic renderer", "pure chart"]
-)
-
+# Tabbed plots 
+tab1, tab2, tab3 = st.tabs(["Rental Prices", "Sales Prices", "2023 Prices by City"])
 with tab1:
-    renderer.explorer()
-
-with tab2:
-    renderer.explorer(default_tab="data")
-
-with tab3:
-    renderer.viewer()
-
-with tab4:
-    st.markdown("### registered per weekday")
+    st.header("State-Wide Median Rent Price Averages in 2023")
     renderer.chart(0)
-    st.markdown("### registered per day")
-    renderer.chart(1)
+with tab2:
+    st.header("State-Wide Median Sale Price Averages in 2023")
+    renderer2.chart(0)
+with tab3:
+    st.header("City-Wide Median Sale Price Averages in 2023")
+    renderer3.chart(0)
+
+
+# Plotting rental trends
+st.header("Interactive Rental Trends")
+
+all_cities = rental_sales['Region Name'].unique()
+selected_cities = st.multiselect('Select Cities', all_cities, default=['New York, NY', 'Los Angeles, CA', 'Chicago, IL', 'Dallas, TX', 'San Francisco, CA'])
+filtered_rentals = rental_sales[rental_sales['Region Name'].isin(selected_cities)]
+
+if not filtered_rentals.empty:
+    plt.figure(figsize=(8, 3))  # Adjust size as needed
+    for city in selected_cities:
+        city_data = filtered_rentals[filtered_rentals['Region Name'] == city]
+        plt.plot(city_data['Date Recorded'], city_data['Monthly Rent'], label=city)
+    plt.title('Trend of Monthly Rent Prices for Selected Major Metro Areas')
+    plt.xlabel('Year')
+    plt.ylabel('Monthly Rent ($)')
+    # Position the legend outside of the plot
+    plt.legend(title='Metro Area', loc='upper left', bbox_to_anchor=(1, 1))
+    plt.grid(True)
+    plt.tight_layout()  # This can help to fit everything within the figure cleanly
+    st.pyplot(plt)
+else:
+    st.write("No data available for the selected cities.")
+
+
+# Predicted Sales Calculator
+df = predicted_sales
+df['Base Date Recorded'] = pd.to_datetime(df['Base Date Recorded'])
+df['Forecast Date'] = pd.to_datetime(df['Forecast Date'])
+
+st.title("Forecasted Growth Finder")
+st.markdown("Forecast Growht % represents the projected percentage change in home values")
+# User input for selecting a state
+selected_state = st.selectbox('Select State', df['State Name'].unique())
+
+# Filter data based on the selected state
+state_data = df[df['State Name'] == selected_state]
+
+# User input for selecting a city from the filtered state data
+if not state_data.empty:
+    selected_city = st.selectbox('Select City', state_data['Region Name'].unique())
+
+    # Filter city data based on the selected city
+    city_data = state_data[state_data['Region Name'] == selected_city]
+
+    # Display the data using HTML
+    if not city_data.empty:
+        st.markdown("### Forecast Growth (%) for selected city:")
+        html = city_data[['Region Name', 'Forecast Date', 'Forecast Growth (%)']].to_html(index=False)
+        st.write(html, unsafe_allow_html=True)
+    else:
+        st.write("No data available for the selected city.")
+else:
+    st.write("No data available for the selected state.")
